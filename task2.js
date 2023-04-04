def4202d8ade32ed8fcf2cba27702297735468c4
tsheporamantso <tshepo.ramantso@outlook.com>
2023-04-04 16:37:02 +0200

    Add reverse string function

---
+++ task2.js
@@ -0,0 +1,10 @@
+const reverseString = (string) => {
+    let newString = "";
+    for (let i = string.length-1; i >= 0; i -= 1){
+        newString += string[i];
+
+    }
+    return newString;
+}
+
+module.exports = reverseString;
